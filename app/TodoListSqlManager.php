49b460166a3b1fc3626b54dacbd4821715225861
miquel <alexandre.miquel.dev@gmail.com>
2025-01-15 01:02:31 +0100

    Handle requests for todolist CRUD

--- app/TodoListSqlManager.php
+++ app/TodoListSqlManager.php
@@ -23,12 +23,12 @@ class TodoListSqlManager
      * Summary of getTasks
      * @return array|bool
      */
-    public function getTasks() {
+    public function getTasks(): array|bool {
         $return = [];
-        $sql = "SELECT * FROM task_status WHERE task_status_id = 99;";
+        $sql = "SELECT * FROM tasks ORDER BY task_creation_date";
         
         try {
-            
+
             $stmt = $this->pdo->prepare($sql);
             $stmt->execute();
 
@@ -40,6 +40,44 @@ class TodoListSqlManager
 
         return $return;
     }
+
+    /**
+     * Summary of createTask
+     * @param mixed $label
+     * @return bool
+     */
+    public function createTask($label) {
+        $return = false;
+        
+        $sql = "INSERT INTO tasks (task_label, task_status_id) VALUES (:task_label, 1)";
+
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindParam(':task_label', $label, PDO::PARAM_STR);
+            $return = $stmt->execute();
+        } catch (PDOException $e) {
+            echo "create Task Error: ". $e->getMessage();
+        }
+
+        return $return;
+    }
+
+    public function deleteTask($task_id) {
+        $return = false;
+        
+        $sql = "DELETE FROM tasks WHERE task_id = :task_id";
+        
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindParam("task_id", $task_id, PDO::PARAM_INT);
+            $return = $stmt->execute();
+        } catch(PDOException $e) {
+            echo "Error from Delete task: ". $e->getMessage();
+        }
+
+        return $return;
+    }
+
 }
 
 ?>
