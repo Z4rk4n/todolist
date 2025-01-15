e92e2875fdfd719e9d91a72a891516998dafc1b7
miquel <alexandre.miquel.dev@gmail.com>
2025-01-15 01:03:22 +0100

    Now u can create / delete task from front page

--- index.php
+++ index.php
@@ -9,7 +9,17 @@ $kernel = new Kernel($config);
 $kernel->init();
 
 $todoListSqlManager = new TodoListSqlManager(pdo: $kernel->getPdo());
-var_dump($todoListSqlManager->getTasks());
+
+if (
+    isset($_POST["new_task_submit"]) 
+    && isset($_POST["new_task_label"]) 
+    && !empty($_POST["new_task_label"])
+    ) {
+        $newTaskLabel = $_POST["new_task_label"];
+        $todoListSqlManager->createTask($newTaskLabel);
+}
+
+$tasks = $todoListSqlManager->getTasks();
 
 ?>
 <!DOCTYPE html>
@@ -17,12 +27,62 @@ var_dump($todoListSqlManager->getTasks());
     <head>
         <meta charset="UTF-8" />
         <title>TodoList</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </head>
     <body>
+        <table>
+            <tr>
+                <th>Label</th>
+                <th></th>
+            </tr>
+        <?php foreach ($tasks as $task) { ?>
+            <tr id="<?php echo $task["task_id"] ?>">
+                <td><?php echo $task["task_label"] ?></td>
+                <td><button name="delete" class="task-delete-btn" data-id="<?php echo $task["task_id"] ?>" onclick="deleteTask(this)">X</button></td>
+            </tr>
+        <?php } ?>         
+        </table>
         <form method="post">
             <input type="text" name="new_task_label" />
             <input type="submit" name="new_task_submit" />
         </form>
+        <script type="text/javascript">
+            function deleteTask(deleteButton) {
+
+                var delete_task_id = deleteButton.getAttribute("data-id");
+                const xhr = new XMLHttpRequest();
+
+                // Prépare une requête GET
+                xhr.open('GET', '/ajax_service.php?action=delete&task_id=' + delete_task_id, true);
+
+                // Gère la réponse
+                xhr.onload = function () {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    console.log('Données reçues :', JSON.parse(xhr.responseText));
+                } else {
+                    console.error(`Erreur : ${xhr.status} - ${xhr.statusText}`);
+                }
+                };
+
+                // Gère les erreurs
+                xhr.onerror = function () {
+                console.error('Erreur réseau.');
+                };
+
+                // Envoie la requête
+                xhr.send();
+
+                td = document.getElementById(delete_task_id);
+                td.remove();
+            
+            }
+            
+            // Pour une requête POST
+            //const postData = { key: 'value' };
+            //xhr.open('POST', 'https://api.example.com/data', true);
+            //xhr.setRequestHeader('Content-Type', 'application/json');
+            //r.send(JSON.stringify(postData));
+        </script>
     </body>
 </html>
 
