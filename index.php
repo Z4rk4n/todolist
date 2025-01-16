2ae95bf712473aa2e58ab019126d4202852ee32f
miquel <alexandre.miquel.dev@gmail.com>
2025-01-16 22:12:49 +0100

    Csss .. It was a nightmare

--- index.php
+++ index.php
@@ -28,25 +28,42 @@ $tasks = $todoListSqlManager->getTasks();
         <meta charset="UTF-8" />
         <title>TodoList</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="web/css/style.css" />
     </head>
     <body>
-        <table>
-            <tr>
-                <th>Label</th>
-                <th></th>
-            </tr>
+        <div id="todolist-items-container">
         <?php foreach ($tasks as $task) { ?>
-            <tr id="<?php echo $task["task_id"] ?>">
-                <td><?php echo $task["task_label"] ?></td>
-                <td><button name="delete" class="task-delete-btn" data-id="<?php echo $task["task_id"] ?>" onclick="deleteTask(this)">X</button></td>
-            </tr>
+            <div id="<?php echo $task["task_id"] ?>" class="todolist-task-item">
+                <div class="todolist-task-label-container">
+                <div class="todolist-task-label">
+                    <?php echo $task["task_label"] ?>
+                </div>
+                </div>
+                <div class="todolist-task-delete-btn-container">
+                    <button name="delete" class="todolist-task-delete-btn" data-id="<?php echo $task["task_id"] ?>" onclick="deleteTask(this)">remove</button>
+                </div>
+            </div>
         <?php } ?>         
-        </table>
+        </div>
         <form method="post">
-            <input type="text" name="new_task_label" />
-            <input type="submit" name="new_task_submit" />
+            <div id="todolist-create-task-containter">
+                <div class="todolist-create-task-input-text-container">
+                    <input type="text" name="new_task_label" class="todolist-create-task-input-text"/>
+                </div>
+                <div class="todolist-task-input-submit-container">
+                    <input type="submit" name="new_task_submit" class="todolist-task-input-submit"/>
+                </div>
+            </div>
         </form>
         <script type="text/javascript">
+
+            // get the scroll height of the window
+            const scrollHeight = document.body.scrollHeight;
+
+            // scroll to the bottom of webpage
+            window.scrollTo(0, scrollHeight);
+
+            // delete task action ( send Ajax request too ajax service hnh )
             function deleteTask(deleteButton) {
 
                 var delete_task_id = deleteButton.getAttribute("data-id");
@@ -82,6 +99,7 @@ $tasks = $todoListSqlManager->getTasks();
             //xhr.open('POST', 'https://api.example.com/data', true);
             //xhr.setRequestHeader('Content-Type', 'application/json');
             //r.send(JSON.stringify(postData));
+        
         </script>
     </body>
 </html>
